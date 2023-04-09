0624e001672d06aa84422b1193dc5c3dd7d55a0d
IT21040962 <it21040962@my.sliit.lk>
2023-04-10 01:49:54 +0530

    serverjs change

--- Backend/server.js
+++ Backend/server.js
@@ -1,59 +1,28 @@
-
-
 const express = require('express');
-
 const mongoose = require('mongoose');
-
 const cors = require('cors');
-
 const dotenv = require('dotenv');
-
 require("dotenv").config();
-
 const app = express();
 
-
-
-
 const port = process.env.PORT || 4000;
-
 const URL = process.env.MONGO_URL;
-
 app.use(cors());
-
 app.use(express.json())
 
-
-
-
 mongoose.connect(URL, {
-
     useNewUrlParser: true,
-
     useUnifiedTopology: true
-
 })
-
-
-
 
 //DB connection
-
 const connection = mongoose.connection;
-
 connection.once("open", () => {
-
     console.log("MongoDB connected");
-
 })
 
-
-
-
 app.listen(port, () => {
-
     console.log("PORT conneted on " + port);
-
 })
 
 //plant route
