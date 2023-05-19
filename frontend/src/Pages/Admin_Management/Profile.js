da1fea8024c758f616a84e3272378a649e1c56c9
it21091452 <sudarsha@live.com>
2023-05-19 13:04:23 +0530

    Errors fixed in profile page

--- frontend/src/Pages/Admin_Management/Profile.js
+++ frontend/src/Pages/Admin_Management/Profile.js
@@ -75,6 +75,7 @@ const Profile = () => {
                     style={{ minWidth: '600px' }}
                 >
                     <Form.Item
+                        label="First Name"
                         name="firstName"
                         rules={[{ required: true, message: 'Please input your first name!' }]}
                     >
@@ -82,6 +83,7 @@ const Profile = () => {
                     </Form.Item>
 
                     <Form.Item
+                        label="Last Name"
                         name="lastName"
                         rules={[{ required: true, message: 'Please input your last name!' }]}
                     >
@@ -89,6 +91,7 @@ const Profile = () => {
                     </Form.Item>
 
                     <Form.Item
+                        label="Email"
                         name="email"
                         rules={[
                             {
